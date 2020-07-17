ecffe1be5f1539a73985c8e2f18031f42919cb31
SeungheeJo <36188903+seungheejo@users.noreply.github.com>
2020-07-17 15:09:28 +0900

    Update PjinvitedlistMapper.java
    주석언어변경 (일본어->한국어)

--- src/main/java/org/jsp/board/service/PjinvitedlistMapper.java
+++ src/main/java/org/jsp/board/service/PjinvitedlistMapper.java
@@ -10,7 +10,7 @@ public interface PjinvitedlistMapper {
 
 	public ArrayList<Pjinvitedlist> invitedList(int pjno);
 
-	// 招待リストとSkettoのメンバーリストのジョイン
+	// 초대리스트와 Sketto의 멤버리스트의 조인
 	public ArrayList<Pjinvitedlist> checkIfJoined(int pjno);
 
 	public int deleteFromInvited(Pjinvitedlist ivt);
